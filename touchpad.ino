0c74b25c82e60c83f09f6df4a317b1b5719e624f
CamilaColussi-KSA <c.colussi@kingston.ac.uk>
2026-04-28 16:00:22 +0100

    Update touchpad.ino

--- touchpad.ino
+++ touchpad.ino
@@ -1,18 +1,15 @@
 int signalPin = 6;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int touchState = digitalRead(signalPin);
   
   if(touchState==1){
   Serial.println("touch detected!");
-  }
-  else{
+  } else {
   Serial.println("no touch detected!");
   }
 }
